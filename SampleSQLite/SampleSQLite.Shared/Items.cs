d7625ef5889804ebf582cd32627ec7198fe20488
miqbal23 <muhamadiqbal@live.co.uk>
2015-05-29 22:43:18 +0700

    added SQLite into solution, Items created as table

---
+++ SampleSQLite/SampleSQLite.Shared/Items.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace SampleSQLite
+{
+    [Table("Items")]
+    class Items
+    {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Score { get; set; }
+    }
+}
